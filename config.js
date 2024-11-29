bd21963ab0912f75296d0d5aba9d16a4f8db781c
Chintan Shah <kathi.shah@gmail.com>
2024-11-28 19:51:46 -0800

    part 2 of the setup

--- config.js
+++
@@ -1,8 +0,0 @@
-// AWS Configuration
-const awsConfig = {
-    region: 'us-west-1',
-    identityPoolId: 'us-west-1:b15a5865-d7e7-4153-9456-7d271afd31d6',
-    bucketName: 'crispvoice-audio-recordings'
-};
-
-export default awsConfig;
