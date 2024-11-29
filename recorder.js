758b161a46daff352b0237a7770315dc02a49cda
Chintan Shah <kathi.shah@gmail.com>
2024-11-28 19:36:06 -0800

    Set up for Github pages

--- recorder.js
+++ recorder.js
@@ -1,4 +1,9 @@
-import awsConfig from './config.js';
+// AWS Configuration
+const awsConfig = {
+    region: 'us-west-1',
+    identityPoolId: 'us-west-1:b15a5865-d7e7-4153-9456-7d271afd31d6',
+    bucketName: 'crispvoice-audio-recordings'
+};
 
 document.addEventListener('DOMContentLoaded', () => {
     const microphoneInfo = document.getElementById('microphoneInfo');
