ddee3905513fdc1685a8968bdf200fe9b90c6119
Chintan Shah <kathi.shah@gmail.com>
2024-12-30 20:15:17 -0800

    Merging to main

--- recorder.js
+++ recorder.js
@@ -1,8 +1,8 @@
 // AWS Configuration
 const awsConfig = {
     region: 'us-west-1',
-    identityPoolId: 'us-west-1:b15a5865-d7e7-4153-9456-7d271afd31d6',
-    bucketName: 'crispvoice-audio-recordings'
+    identityPoolId: 'your-identity-pool-id',
+    bucketName: 'your-s3-bucket-name'
 };
 
 document.addEventListener('DOMContentLoaded', () => {
